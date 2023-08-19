4bad04ed9d1f1903e01ca8f46340bfe73d679b06
Dionysis <dionysisterzios@gmail.com>
2023-08-19 14:32:17 +0300

    Start again the project

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
-const gameBoard = (() => {
-    const form = document.querySelector('#form');
-    const players = [];
-    const handleSubmit = event => {
-        // event.preventDefault();
+const Gameboard = {
+    gameboard: []
+};
 
-        const formData = new FormData(form);
-        const Player = (playerName, playerMark) => {
-            return { playerName, playerMark };
-        }
+console.log(Gameboard);
 
-        for (const pair of formData.entries()) {
-            const playerNumber = pair[0];
-            const playerName = pair[1];
 
-            const player = playerNumber === 'playerOne'
-                ? Player(playerName, 'X')
-                : Player(playerName, 'O');
 
-            players.push(player);
-        }
 
-        console.log(players);
-    }
 
-    form.addEventListener('submit', handleSubmit);
-})();
+
+
+// const gameBoard = (() => {
+//     const form = document.querySelector('#form');
+//     const players = [];
+//     const handleSubmit = (event) => {
+//         event.preventDefault();
+
+//         const formData = new FormData(form);
+//         const Player = (playerName, playerMark) => {
+//             return { playerName, playerMark };
+//         }
+
+//         for (const pair of formData.entries()) {
+//             const playerNumber = pair[0];
+//             const playerName = pair[1];
+//             const player = playerNumber === 'playerOne'
+//                 ? Player(playerName, 'X')
+//                 : Player(playerName, 'O');
+
+//             players.push(player);
+//         }
+//     }
+
+//     form.addEventListener('submit', handleSubmit);
+// })();
+
