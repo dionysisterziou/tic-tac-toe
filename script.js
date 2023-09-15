4a51d5d90bd807b03379396fd5235914e85ae0ed
Dionysis <dionysisterzios@gmail.com>
2023-09-15 15:38:14 +0300

    Change player

--- script.js
+++ script.js
@@ -1,36 +1,8 @@
 const gameBoard = (() => {
-    // let turn = 'playerOne';
+    let player = 1;
     const gameBoard = [];
-    // const renderBoard = () => {
-    //     const squares = document.querySelectorAll('.square');
-
-    //     squares.forEach(square => {
-    //         const squareNumber = square.dataset.number;
-
-    //         gameBoard.forEach(turn => {
-    //             if (squareNumber === turn.square) {
-    //                 square.textContent = turn.marker;
-    //             }
-    //         })
-    //     })
-    // }
     const renderBoard = (square, marker) => {
-        // const squares = document.querySelectorAll('.square');
-
-        // squares.forEach(square => {
-        //     const squareNumber = square.dataset.number;
-
-        //     gameBoard.forEach(turn => {
-        //         if (squareNumber === turn.square) {
-                    // square.textContent = turn.marker;
-                    // console.log(square);
-                    // console.log(squareNumber);
-                    // console.log(marker);
-                    // console.log(gameBoard);
-                    square.textContent = marker;
-                // }
-            // })
-        // })
+        square.textContent = marker;
     }
     const addMarker = (marker, square) => {
         gameBoard.push({
@@ -38,26 +10,32 @@ const gameBoard = (() => {
             square
         });
     }
+    const changePlayer = () => {
+        if (player === 1) {
+            player = 2;
+            return 'X';
+        } else {
+            player = 1;
+            return 'O';
+        }
+    }
 
     return {
-        /* renderBoard, */
         renderBoard,
-        addMarker /*,
-         gameBoard */
+        addMarker,
+        changePlayer
     };
 })();
 
 const displayController = (() => {
-    // const renderBoard = gameBoard.renderBoard();
-
-    /* Testing */
     const playRound = () => {
         const squares = document.querySelectorAll('.square');
 
         squares.forEach(square => {
             square.addEventListener('click', () => {
                 const squarePlace = square.dataset.place;
-                // const marker = 'X';
+                let marker = gameBoard.changePlayer();
+
 
                 gameBoard.addMarker(marker, squarePlace);
                 gameBoard.renderBoard(square, marker);
@@ -68,11 +46,10 @@ const displayController = (() => {
     return {
         playRound
     }
-})(/*gameBoard.renderBoard */);
+})();
 
 const Player = (name) => {
     return { name };
 }
 
-// gameBoard.addMarker('X');
 displayController.playRound();
