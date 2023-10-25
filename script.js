9eae931481972b280a99f58cbf297f964beff9fa
Dionysis <dionysisterzios@gmail.com>
2023-10-25 14:12:19 +0300

    Refactor
    Step 2 from Tic-Tac-Toe project.

--- script.js
+++ script.js
@@ -1,178 +1,193 @@
-const gameBoard = (() => {
-    let gameBoard = [];
-    const form = document.querySelector('#form');
-    const board = document.querySelector('#board');
-    const restart = document.querySelector('#restart');
-    const players = [];
+const GameBoard = (() => {
+    const gameBoard = [];
 
-    form.addEventListener('submit', handleSubmit);
-
-    function handleSubmit(e) {
-        e.preventDefault();
-
-        const formData = new FormData(e.target);
-
-        for (const pair of formData.entries()) {
-            const number = pair[0];
-            const name = pair[1];
-
-
-            if (number === 'playerOne') {
-                const marker = 'X';
-                const player = createPlayer(name, marker);
-
-                players.push(player);
-            } else {
-                const marker = 'O';
-                const player = createPlayer(name, marker);
-
-                players.push(player);
-            }
+    return {
+        getGameBoard: function () {
+            return gameBoard;
         }
-
-        board.classList.remove('hide');
-        restart.classList.remove('hide')
-        form.classList.add('hide');
     }
+})
 
-    const createPlayer = (name, marker) => {
-        return {
-            name,
-            marker
-        };
-    }
-    const renderBoard = (square, marker) => {
-        square.textContent = marker;
-    };
-    const addMarker = (marker, squarePlace) => {
-        gameBoard.push({
-            marker,
-            squarePlace
-        });
-    };
-    const checkResult = (restarted) => {
-        const conditions = [
-            [1, 2, 3],
-            [1, 4, 7],
-            [1, 5, 9],
-            [2, 5, 8],
-            [3, 5, 7],
-            [3, 6, 9],
-            [4, 5, 6],
-            [7, 8, 9]
-        ]
+const DisplayController = (function () {})();
 
-        // console.log(restarted);
-
-        return conditions.some(function (condition, index) {
-            let winningValues = gameBoard.filter((turn) => condition.includes(turn.squarePlace));
-            console.log(winningValues);
-            if (restarted === true) {
-                winningValues = [];
-            }
-            const isSameMarker = winningValues.every(({ marker }) => marker === winningValues[0].marker);
-
-            if (winningValues.length === 3) { // Because the code will run always
-                if (isSameMarker) {
-                    if (winningValues[0].marker === 'X') {
-                        console.log('Player 1 is the winner.');
-                        return true;
-                    } else {
-                        console.log('Player 2 is the winner.');
-                        return true;
-                    }
-                } else if (index === 7 && gameBoard.length === 9) {
-                    console.log('Tie!');
-                    return true;
-                }
-            }
-        })
-    }
-
+function createPlayer(name, marker) {
+    let playerName = name;
+    let playerMarker = marker;
+    
     return {
-        renderBoard,
-        addMarker,
-        checkResult,
-        players,
-        gameBoard
-    };
-})();
-
-const displayController = (() => {
-    let turn = 1;
-    let isOver = false;
-    let squares = document.querySelectorAll('.square');
-    let restart = document.querySelector('#restart');
-    let restarted = false;
-
-    restart.addEventListener('click', restartGame);
-
-    const playRound = () => {
-        squares.forEach(square => {
-            square.addEventListener('click', () => {
-                if (square.textContent === '') {
-                    console.log(turn);
-                    if (!isOver) {
-                        const squarePlace = parseInt(square.dataset.place);
-                        if (turn % 2 !== 0) {
-                            let player = gameBoard.players[0];
-                            let marker = player.marker;
-
-                            gameBoard.addMarker(marker, squarePlace);
-                            turn++;
-
-                            // // console.log(restarted)
-                            // if (turn >= 5) {
-                            //     // console.log(restarted);
-                            //     if (gameBoard.checkResult(restarted)) {
-                            //         isOver = true;
-                            //         restarted = false;
-                            //     }
-                            // }
-                            gameBoard.renderBoard(square, marker);
-                        } else {
-                            let player = gameBoard.players[1];
-                            let marker = player.marker;
-
-                            gameBoard.addMarker(marker, squarePlace);
-                            turn++;
-
-                            // if (turn >= 5) {
-                            //     if (gameBoard.checkResult()) {
-                            //         isOver = true;
-                            //     };
-                            // }
-                            gameBoard.renderBoard(square, marker);
-                        }
-
-                        // console.log(restarted)
-                        if (turn > 5) {
-                            // console.log(restarted);
-                            if (gameBoard.checkResult(restarted)) {
-                                isOver = true;
-                                restarted = false;
-                            }
-                        }
-                    }
-                }
-            });
-        });
+      getPlayerName: function () {
+        return playerName;
+      },
+      getPlayerMarker: function () {
+        return playerMarker;
+      }
     }
+  }
 
-    function restartGame() {
-        isOver = false;
-        restarted = true;
-        gameBoard.gameBoard = [];
-        turn = 1;
 
-        squares.forEach(square => {
-            square.textContent = '';
-        })
-    }
+// const gameBoard = (() => {
+//     const gameBoard = [];
+//     const players = [];
+//     // let turn = 1;
+//     const form = document.querySelector('#form');
+//     const board = document.querySelector('#board');
+//     const restart = document.querySelector('#restart');
 
-    return {
-        playRound
-    }
-})();
+//     const getGameBoard = () => gameBoard;
+//     // const getTurn = () => turn;
+//     // const changeTurn = () => turn++;
 
-displayController.playRound();
+//     form.addEventListener('submit', handleSubmit);
+
+//     function handleSubmit(e) {
+//         e.preventDefault();
+
+//         const formData = new FormData(e.target);
+
+//         for (const pair of formData.entries()) {
+//             const number = pair[0];
+//             const name = pair[1];
+//             let marker = '';
+
+//             if (number === 'playerOne') {
+//                 marker = 'X';
+//             } else {
+//                 marker = 'O';
+//             }
+
+//             const player = createPlayer(name, marker);
+
+//             players.push(player);
+//         }
+
+//         console.log(players);
+
+//         board.classList.remove('hide');
+//         restart.classList.remove('hide')
+//         form.classList.add('hide');
+//     }
+
+//     const createPlayer = (name, marker) => {
+//         return {
+//             name,
+//             marker
+//         };
+//     }
+//     const renderBoard = (square, marker) => {
+//         square.textContent = marker;
+//     };
+//     const addMarker = (marker, squarePlace) => {
+//         gameBoard.push({
+//             marker,
+//             squarePlace
+//         });
+//     };
+//     const checkResult = () => {
+//         const conditions = [
+//             [1, 2, 3],
+//             [1, 4, 7],
+//             [1, 5, 9],
+//             [2, 5, 8],
+//             [3, 5, 7],
+//             [3, 6, 9],
+//             [4, 5, 6],
+//             [7, 8, 9]
+//         ]
+
+//         return conditions.some(function (condition, index) {
+//             const winningValues = gameBoard.filter((turn) => condition.includes(turn.squarePlace));
+//             const isSameMarker = winningValues.every(({ marker }) => marker === winningValues[0].marker);
+
+//             if (winningValues.length === 3) { // Because the code will run always
+//                 // console.log(winningValues);
+//                 if (isSameMarker) {
+//                     if (winningValues[0].marker === 'X') {
+//                         console.log('Player 1 is the winner.');
+//                         return true;
+//                     } else {
+//                         console.log('Player 2 is the winner.');
+//                         return true;
+//                     }
+//                 } else if (index === 7 && gameBoard.length === 9) {
+//                     console.log('Tie!');
+//                     return true;
+//                 }
+//             }
+//         })
+//     }
+
+//     return {
+//         renderBoard,
+//         addMarker,
+//         checkResult,
+//         players,
+//         gameBoard, 
+//         getGameBoard // Testing begins
+//         /*  
+//         getTurn,
+//         changeTurn
+//         */
+//     };
+// })();
+
+// const displayController = (() => {
+//     let turn = 1;
+//     let isOver = false;
+//     let squares = document.querySelectorAll('.square');
+//     let restart = document.querySelector('#restart');
+    
+//     restart.addEventListener('click', restartGame);
+
+//     const playRound = () => {
+//         squares.forEach(square => {
+//             square.addEventListener('click', () => {
+//                 if (square.textContent === '') {
+//                     console.log(turn);
+//                     console.log(gameBoard.getGameBoard());
+//                     if (!isOver) {
+//                         const squarePlace = parseInt(square.dataset.place);
+//                         if (turn % 2 !== 0) {
+//                             let player = gameBoard.players[0];
+//                             let marker = player.marker;
+
+//                             gameBoard.addMarker(marker, squarePlace);
+//                             turn++;
+//                             gameBoard.renderBoard(square, marker);
+//                         } else {
+//                             let player = gameBoard.players[1];
+//                             let marker = player.marker;
+
+//                             gameBoard.addMarker(marker, squarePlace);
+//                             turn++;
+//                             gameBoard.renderBoard(square, marker);
+//                         }
+
+//                         if (turn > 5) {
+//                             if (gameBoard.checkResult()) {
+//                                 isOver = true;
+//                             }
+//                         }
+//                     }
+//                 }
+//             });
+//         });
+//     }
+
+//     function restartGame() {
+//         // console.log(gameBoard.gameBoard);
+//         isOver = false;
+//         gameBoard.gameBoard = [];
+//         turn = 1;
+
+//         squares.forEach(square => {
+//             square.textContent = '';
+//         })
+//     }
+
+//     return {
+//         playRound,
+//         restartGame
+//     }
+// })();
+
+// displayController.playRound();
